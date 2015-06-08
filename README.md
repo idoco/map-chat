3a8401a0ae9a73ae795824b4d0376eefb310f82e
“idoco” <ido.c.mail@gmail.com>
2015-06-08 20:48:57 +0300

    Real first commit. Only simple chat.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # MapChat
+
+Steps:
+- Have Vert.x (2.1.5) installed in path.
+- Run the server with "vertx run ChatVerticle.java".
+- Chat.
---
+++ main.js
@@ -0,0 +1,30 @@
+eb = new vertx.EventBus("http://localhost:8080/chat");
+
+eb.onopen = function () {
+    var topic = "main";
+
+    subscribe(topic);
+
+    var input = $(".input");
+    input.keyup(function (e) {
+        if (e.keyCode == 13) {
+            publish(topic, input.val());
+            input.val('');
+        }
+    });
+};
+
+function publish(address, message) {
+    if (eb) {
+        var json = {text: message};
+        eb.publish(address, json);
+    }
+}
+
+function subscribe(address) {
+    if (eb) {
+        eb.registerHandler(address, function (msg) {
+            $(".chat").append(msg.text + "</br>");
+        });
+    }
+}
