823bda7eee0ac220a43cb2db6b776813b6c1027b
“idoco” <ido.c.mail@gmail.com>
2015-07-02 19:40:00 +0300

    deploy script updated with blacklist file

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ sudo kill -9 $(pgrep -f "ChatVerticle")
 rm -f ChatVerticle.java
 
 # download the server code
-wget -N https://raw.githubusercontent.com/idoco/map-chat/gh-pages/ChatVerticle.java
+wget -N https://raw.githubusercontent.com/idoco/map-chat/gh-pages/server/ChatVerticle.java
 
 # run chat server and log to nohup
-nohup vert.x-2.1.5/bin/vertx run ChatVerticle.java &
+nohup vert.x-2.1.5/bin/vertx run ChatVerticle.java -conf conf.json &
 
