517b92a18eafdc46c2a7107508908ae866540fdb
“idoco” <ido.c.mail@gmail.com>
2015-09-01 13:46:05 +0300

    get location issue 10 - random location issue

--- javascript/map.js
+++ javascript/map.js
@@ -95,8 +95,8 @@ function onPositionError(err) {
     var from = -180;
     var to = 180;
     // whole world random (360 * Math.random() + 180).toFixed(3) * 1;
-    var lat = (200 * Math.random() + 100).toFixed(3) * 1;
-    var lng = (200 * Math.random() + 100).toFixed(3) * 1;
+    var lat = (200 * Math.random() - 100).toFixed(3) * 1;
+    var lng = (200 * Math.random() - 100).toFixed(3) * 1;
     onFirstPosition({
         "coords" : {
             latitude : lat,
